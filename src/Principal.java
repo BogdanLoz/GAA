c375a88dc87ae8ffd267660ee54cd738e8ff1fd7
UserPL135Pc16 <UserPL135Pc16@SLFRPL135PC16.ensino.atec.local>
2016-10-26 11:34:23 +0100

    Commit

--- src/Principal.java
+++ src/Principal.java
@@ -10,7 +10,39 @@ public class Principal {
 		
 		
 		System.out.println("Hello Class!!!");
-
+		
+		
+		int quotas 		= 300,
+			recBaile	= 200,
+			recBar 		= 250;
+				
+		int totalReceitas = quotas + recBaile + recBar;	
+			
+			System.out.println("Angariou : " + totalReceitas + "€ ");
+			
+			
+			
+			
+			int agua = 100,
+				luz = 60,
+				telefone = 70,
+				carro = 150;
+	
+	
+			int totalDespesas = agua + luz + telefone + carro;
+			
+			System.out.println("Gastou: " + totalDespesas + " € ");
+	
+	
+			int totalProfit = totalReceitas - totalDespesas;
+				
+			System.out.println("Teve um profit de " + totalProfit + " € , podes comprar um presente :)");
+	
+	
+	
 	}
+	
+	
+	
 
 }
